e4873e4975957c1e8e4be661f53ed963d2ca257d
ivojumelet <144031293+ivojumelet@users.noreply.github.com>
2023-10-13 10:39:46 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-let budget = 100;
-let productprijs = 60;
-let saldo = document.getElementById ("Resultaat")
-if (budget => productprijs)
-let over = budget - productprijs
-saldo.innerHTML = "U heeft genoeg geld!" + over + "is er over van :" + budget + "euro";
-saldo.style.color = "green"
 
+    let budget = 100;
+    //parseFloat(prompt("Wat is het saldo?","));
+    
+    let productprijs = 60;
+    //let productprijs=parseFloat=prompt("Wat is de prijs van het product?", ""));
+    let saldo = document.getElementById("saldo");
+    
+    if (budget > productprijs) {
+      let over = budget - productprijs;
+      saldo.innerHTML = "U heeft genoeg geld! " + over + " is er over van: "  + budget + "euro";
+      saldo.style.color = "green";
+    } 
     else {
-        saldo.innerHTML= "Helaas, u heeft te weinig geld!";
-        saldo.style.color = "red";
+      saldo.innerHTML = "Helaas, u heeft te weinig geld!";
+      saldo.style.color = "red";
     }
